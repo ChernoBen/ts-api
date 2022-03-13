7aa42a4d50a58c6a3ee8159d869b6f30a0bf7748
ChernoBen <bneto@computacao.cua.ufmt.br>
2022-03-13 09:40:20 -0400

    test: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/presentation/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,10 @@
+//import {EmailValidator} from '../protocols/email-validator'
+import {EmailValidatorAdapter} from './email-validator'
+
+describe('Email validator adapter',()=>{
+    test('Should return false if validator returns false',()=>{
+        const sut = new EmailValidatorAdapter()
+        const isValid = sut.isValid('invalid_email@mail.com')
+        expect(isValid).toBe(false)
+    })
+})
