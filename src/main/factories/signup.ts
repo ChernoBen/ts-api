1818b210798caf89a78c74f1e05d1e6b63f647ff
ChernoBen <bneto@computacao.cua.ufmt.br>
2022-03-13 09:50:21 -0400

    refactor: add validator lib

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,10 @@
+import {SignUpController} from '../../presentation/controllers/signup/signup'
+//import {} from '../'
+
+/*
+export const makeSignUpController = (): SignUpController => {
+    const 
+    const signupController = new SignUpController()
+    return signupController
+}
+*/
